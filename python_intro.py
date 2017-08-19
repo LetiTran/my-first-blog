4cc78406b0325b26c80a10bf6c6f3bd4966822e7
LeticiaTrangit config --global user.email leticiastran@gmail.comexit()git config --global user.name LeticiaTran <leticiastran@gmail.com>
2017-08-19 11:55:58 -0700

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,47 @@
+ # print('Hello world!')
+# if  5 > 2:
+#      print('5 is indeed greater than 2')
+#  else:
+#      print('5 is not greater than 2')
+
+
+# name = 'Sonja'
+# age = 17
+#
+# if age < 21:
+#     print('You are not allowed to see this')
+# elif name == 'Ola':
+#     print('Hey Ola!')
+# elif name == 'Sonja':
+#     print ('Hey Sonja!')
+# elif name == 'Marie':
+#     print ('Hey Marie!')
+# elif name == 'Magda':
+#     print ('Hey Magda!')
+# else:
+#     print ('Hey anonymous')
+
+# def hi(name):
+#     print('Hi ' + name + '!')
+#
+#     # if name == 'Ola':
+#     #     print('Hi Ola')
+#     # elif name == 'Sonja':
+#     #     print('Hi Sonja!')
+#     # else:
+#     #     print('Hi anonymous!')
+#     # print('How are you?')
+# hi("Leti")
+
+
+
+def hi(name):
+    print('Hi ' + name + '!')
+
+girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'Leti']
+for name in girls:
+    hi(name)
+    print('Next girl')
+for i in range(1, 6):
+    print(i)
+    
